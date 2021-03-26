637fa7366ab9804fe849b92c56777c3cac10c173
Kothing <39289309+kothing@users.noreply.github.com>
2021-03-26 17:09:25 +0800

    Update index.php

--- infinite/install/index.php
+++ infinite/install/index.php
@@ -117,21 +117,8 @@ if (!isset($license_code)) {
                                             <h1 class="step-title">Start</h1>
                                             <div class="form-group">
                                                 <div class="alert alert-success" role="alert" style="font-size: 15px;">
-                                                    If you don't have a <b>license code</b>, please go to our support system and generate your license code. You can easily manage your licenses through this system and you can contact us by opening a ticket for any problems.
-                                                    <br><br>
-                                                    If you see "Invalid License Code" error, please read this article:&nbsp;<a href="https://codingest.net/problems-solutions?content=145" target="_blank"><strong>Invalid License Code</strong></a>
+                                                    Infinite is a multi-purpose blog-magazine script. It has clean, responsive and user-friendly design. You can manage your posts, custom pages, categories, user comments, advanced settings and contact messages with its powerful Admin panel. Also it has a useful ad management system. You can manage your ad spaces with this system. It is secured, seo optimized, fast and easy to use.
                                                 </div>
-                                            </div>
-                                            <div class="form-group text-center">
-                                                <a href="https://t.cn/A6wUfC8X" target="_blank" class="btn btn-success btn-custom">NULLED</a>
-                                            </div>
-                                            <div class="form-group text-center">
-                                                <strong><a href="https://www.youtube.com/watch?v=HO56846mW0A" target="_blank">Watch the installation video</a></strong>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="email">License Code</label>
-                                                <textarea name="license_code" class="form-control form-input" style="resize: vertical; height: 100px;line-height: 24px;padding: 10px;" placeholder="Enter License Code" required><?php echo $license_code; ?></textarea>
-                                                <small style="margin-top: 10px;display: block">*You need to enter your license code to this field (<strong>not your purchase code</strong>).</small>
                                             </div>
                                         </div>
                                     </div>
